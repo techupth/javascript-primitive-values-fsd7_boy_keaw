ac977e27df145f8b6d5fbf7b2cf55236b23aeb02
FSD7-Boy <boyzasad01@gmail.com>
2024-09-30 14:06:14 +0700

    feat:assignment_2

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,10 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = totalPrice * (25 / 100);
+let priceFinal = totalPrice - discount;
+
+console.log(totalPrice);
+console.log(discount);
+console.log(priceFinal);
